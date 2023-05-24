6d32eb354333eb0d329ea71625191f9bb2ba0f38
double-hope <nadia.prohorchuk@gmail.com>
2023-05-24 10:40:45 +0300

    implement Repository and UnitOfWork pattern

---
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Category.Add(category);
+                unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Category.Update(category);
+                unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            unitOfWork.Category.Remove(category);
+            unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
